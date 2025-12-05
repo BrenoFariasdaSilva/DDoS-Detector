00a55a083031bc6fb6ab245e549cb5b76a6f9c1e
Breno Farias <breno.vicio78@gmail.com>
2025-12-05 13:27:07 -0300

    REFACTOR: Updating description message in the progress bar in genetic_algorithm.py

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -239,9 +239,9 @@ def update_progress_bar(progress_bar, dataset_name, csv_path, pop_size=None, max
          csv_filename = csv_basename # Use only basename
       
       if run_str: # If run string is provided
-         base = f"{BackgroundColors.CYAN}{dataset_name}{BackgroundColors.GREEN}:{BackgroundColors.CYAN}{csv_filename}{BackgroundColors.GREEN}: {run_str}{Style.RESET_ALL}" # Base description with run info
+         base = f"{BackgroundColors.CYAN}{dataset_name}{BackgroundColors.GREEN} - {BackgroundColors.CYAN}{csv_filename}{BackgroundColors.GREEN}: {run_str}{Style.RESET_ALL}" # Base description with run info
       else: # If no run string
-         base = f"{BackgroundColors.CYAN}{dataset_name}{BackgroundColors.GREEN}:{BackgroundColors.CYAN}{csv_filename}{Style.RESET_ALL}" # Base description without run info
+         base = f"{BackgroundColors.CYAN}{dataset_name}{BackgroundColors.GREEN} - {BackgroundColors.CYAN}{csv_filename}{Style.RESET_ALL}" # Base description without run info
       
       details = [] # List to hold detail strings (pop, gen)
       if pop_size is not None: # If population size is provided
