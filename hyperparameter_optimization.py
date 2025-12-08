54f2c147951188d7ae5778d8b4cfc97127cf6eaa
Breno Farias <breno.vicio78@gmail.com>
2025-12-08 15:47:53 -0300

    REFACTOR: Updating the base logic in main function in hyperparameter_optimization.py

--- hyperparameter_optimization.py
+++ hyperparameter_optimization.py
@@ -512,13 +512,22 @@ def main():
    :return: None
    """
 
-   print(f"{BackgroundColors.CLEAR_TERMINAL}{BackgroundColors.BOLD}{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}Main Template Python{BackgroundColors.GREEN} program!{Style.RESET_ALL}", end="\n\n") # Output the welcome message
+   print(f"{BackgroundColors.CLEAR_TERMINAL}{BackgroundColors.BOLD}{BackgroundColors.GREEN}Welcome to the {BackgroundColors.CYAN}Classifiers Hyperparameters Optimization{BackgroundColors.GREEN} program!{Style.RESET_ALL}", end="\n\n") # Output the welcome message
    start_time = datetime.datetime.now() # Get the start time of the program
-   
-   # Your code goes here
+
+   input_path = "./Datasets/CICDDoS2019/01-12/" # Path to the input dataset directory
+   files_to_process = get_files_to_process(input_path, file_extension=".csv") # Get list of CSV files to process
+   files_to_process = ["./Datasets/CICDDoS2019/01-12/DrDoS_DNS.csv"] # TEMPORARY: Process only DrDoS_DNS.csv for testing
+
+   if not files_to_process: # If no files were found to process
+      print(f"{BackgroundColors.RED}No CSV files found in the specified directory: {BackgroundColors.CYAN}{input_path}{Style.RESET_ALL}")
+      return # Exit the main function
+
+   for csv_path in files_to_process: # Process each CSV file
+      pass # Placeholder for processing each file (to be implemented)
 
    finish_time = datetime.datetime.now() # Get the finish time of the program
-   print(f"{BackgroundColors.GREEN}Start time: {BackgroundColors.CYAN}{start_time.strftime('%d/%m/%Y - %H:%M:%S')}\n{BackgroundColors.GREEN}Finish time: {BackgroundColors.CYAN}{finish_time.strftime('%d/%m/%Y - %H:%M:%S')}\n{BackgroundColors.GREEN}Execution time: {BackgroundColors.CYAN}{calculate_execution_time(start_time, finish_time)}{Style.RESET_ALL}") # Output the start and finish times
+   print(f"\n{BackgroundColors.GREEN}Start time: {BackgroundColors.CYAN}{start_time.strftime('%d/%m/%Y - %H:%M:%S')}\n{BackgroundColors.GREEN}Finish time: {BackgroundColors.CYAN}{finish_time.strftime('%d/%m/%Y - %H:%M:%S')}\n{BackgroundColors.GREEN}Execution time: {BackgroundColors.CYAN}{calculate_execution_time(start_time, finish_time)}{Style.RESET_ALL}") # Output the start and finish times
    print(f"\n{BackgroundColors.BOLD}{BackgroundColors.GREEN}Program finished.{Style.RESET_ALL}") # Output the end of the program message
 
    atexit.register(play_sound) if RUN_FUNCTIONS["Play Sound"] else None # Register the play_sound function to be called when the program finishes
