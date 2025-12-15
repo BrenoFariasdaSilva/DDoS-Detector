ebff76858e06c0e8e16c85a9e82ad730e9368109
Breno Farias <breno.vicio78@gmail.com>
2025-12-15 10:47:13 -0300

    FEAT: Adding the CICIDS2017 Dataset to download_datasets.sh

--- download_datasets.sh
+++ download_datasets.sh
@@ -3,18 +3,33 @@
 # Permissions: chmod +x download_datasets.sh
 # Run: ./download_datasets.sh
 
-# Create directory structure
-mkdir -p Datasets/CICDDoS2019
-cd Datasets/CICDDoS2019 || exit
+set -e
 
-# URLs
+# =========================
+# CICDDoS2019
+# =========================
+mkdir -p Datasets/CICDDoS2019
+cd Datasets/CICDDoS2019 || exit 1
+
 URL1="http://cicresearch.ca/CICDataset/CICDDoS2019/Dataset/CSVs/CSV-01-12.zip"
 URL2="http://cicresearch.ca/CICDataset/CICDDoS2019/Dataset/CSVs/CSV-03-11.zip"
 
-# Download ZIP files
-wget "$URL1"
-wget "$URL2"
+wget -c "$URL1"
+wget -c "$URL2"
 
-# Extract both
-unzip CSV-01-12.zip
-unzip CSV-03-11.zip
+unzip -o CSV-01-12.zip
+unzip -o CSV-03-11.zip
+
+cd ../../
+
+# =========================
+# CIC-IDS-2017
+# =========================
+mkdir -p Datasets/CICIDS2017
+cd Datasets/CICIDS2017 || exit 1
+
+URL3="http://cicresearch.ca/CICDataset/CIC-IDS-2017/Dataset/CIC-IDS-2017/CSVs/GeneratedLabelledFlows.zip"
+
+wget -c "$URL3"
+
+unzip -o GeneratedLabelledFlows.zip
