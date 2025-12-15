0c7fa5956c8f4ec6b3851ed0a3564f125d7a7509
Breno Farias <breno.vicio78@gmail.com>
2025-12-15 10:51:00 -0300

    FEAT: Update workflow with multi-dataset support in download_datasets.sh

--- download_datasets.sh
+++ download_datasets.sh
@@ -3,33 +3,43 @@
 # Permissions: chmod +x download_datasets.sh
 # Run: ./download_datasets.sh
 
-set -e
+set -e # Exit on error
 
-# =========================
-# CICDDoS2019
-# =========================
-mkdir -p Datasets/CICDDoS2019
-cd Datasets/CICDDoS2019 || exit 1
+# CONFIGURATION: Dataset URLs and Target Directories (Comment/Uncomment as needed)
+declare -A DATASET_URLS=(
+	# CICDDoS2019
+	["CICDDoS2019_CSV_01_12"]="http://cicresearch.ca/CICDataset/CICDDoS2019/Dataset/CSVs/CSV-01-12.zip"
+	["CICDDoS2019_CSV_03_11"]="http://cicresearch.ca/CICDataset/CICDDoS2019/Dataset/CSVs/CSV-03-11.zip"
 
-URL1="http://cicresearch.ca/CICDataset/CICDDoS2019/Dataset/CSVs/CSV-01-12.zip"
-URL2="http://cicresearch.ca/CICDataset/CICDDoS2019/Dataset/CSVs/CSV-03-11.zip"
+	# CIC-IDS-2017
+	["CICIDS2017"]="http://cicresearch.ca/CICDataset/CIC-IDS-2017/Dataset/CIC-IDS-2017/CSVs/GeneratedLabelledFlows.zip"
+)
 
-wget -c "$URL1"
-wget -c "$URL2"
+declare -A DATASET_DIRS=(
+	["CICDDoS2019_CSV_01_12"]="Datasets/CICDDoS2019"
+	["CICDDoS2019_CSV_03_11"]="Datasets/CICDDoS2019"
+	["CICIDS2017"]="Datasets/CICIDS2017"
+)
 
-unzip -o CSV-01-12.zip
-unzip -o CSV-03-11.zip
+# DOWNLOAD AND EXTRACT DATASETS
+for DATASET in "${!DATASET_URLS[@]}"; do
+	URL="${DATASET_URLS[$DATASET]}"
+	TARGET_DIR="${DATASET_DIRS[$DATASET]}"
+	ZIP_NAME="$(basename "$URL")"
 
-cd ../../
+	echo "==> Setting up dataset: $DATASET"
+	echo "    From: $URL"
+	echo "    To:   $TARGET_DIR/$ZIP_NAME"
+	mkdir -p "$TARGET_DIR"
+	echo "==> Changing to directory $TARGET_DIR"
+	cd "$TARGET_DIR" || exit 1
 
-# =========================
-# CIC-IDS-2017
-# =========================
-mkdir -p Datasets/CICIDS2017
-cd Datasets/CICIDS2017 || exit 1
+	echo "==> Downloading $ZIP_NAME" 
+	wget -c "$URL"
+	echo "==> Extracting $ZIP_NAME"
+	unzip -o "$ZIP_NAME"
 
-URL3="http://cicresearch.ca/CICDataset/CIC-IDS-2017/Dataset/CIC-IDS-2017/CSVs/GeneratedLabelledFlows.zip"
+	cd - >/dev/null
+done
 
-wget -c "$URL3"
-
-unzip -o GeneratedLabelledFlows.zip
+echo "All selected datasets downloaded successfully."
