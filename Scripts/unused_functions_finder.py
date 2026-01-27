b458161d25e6a939f2144448bd54c88013badcc9
Breno Farias da Silva <breno.vicio78@gmail.com>
2026-01-27 19:16:52 -0300

    REFACTOR: Minor change in ./unused_functions_finder.py

--- Scripts/unused_functions_finder.py
+++ Scripts/unused_functions_finder.py
@@ -169,6 +169,20 @@ def verbose_output(true_string="", false_string=""):
     elif false_string != "":  # If the false_string is set
         print(false_string)  # Output the false statement string
 
+def calculate_execution_time(start_time, finish_time):
+    """
+    Calculates the execution time between start and finish times and formats it as hh:mm:ss.
+
+    :param start_time: The start datetime object
+    :param finish_time: The finish datetime object
+    :return: String formatted as hh:mm:ss representing the execution time
+    """
+
+    delta = finish_time - start_time  # Calculate the time difference
+    hours, remainder = divmod(delta.seconds, 3600)  # Calculate the hours, minutes and seconds
+    minutes, seconds = divmod(remainder, 60)  # Calculate the minutes and seconds
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"  # Format the execution time
+
 
 def verify_filepath_exists(filepath):
     """
@@ -183,21 +197,6 @@ def verify_filepath_exists(filepath):
     )  # Output the verbose message
 
     return os.path.exists(filepath)  # Return True if the file or folder exists, False otherwise
-
-
-def calculate_execution_time(start_time, finish_time):
-    """
-    Calculates the execution time between start and finish times and formats it as hh:mm:ss.
-
-    :param start_time: The start datetime object
-    :param finish_time: The finish datetime object
-    :return: String formatted as hh:mm:ss representing the execution time
-    """
-
-    delta = finish_time - start_time  # Calculate the time difference
-    hours, remainder = divmod(delta.seconds, 3600)  # Calculate the hours, minutes and seconds
-    minutes, seconds = divmod(remainder, 60)  # Calculate the minutes and seconds
-    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"  # Format the execution time
 
 
 def play_sound():
