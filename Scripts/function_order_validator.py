7010a8303b1499c2032c5ce937b69b1d6d100003
Breno Farias da Silva <breno.vicio78@gmail.com>
2026-01-27 21:08:34 -0300

    FIX: Fixing the Logger Class Imports in ./function_order_validator.py

--- Scripts/function_order_validator.py
+++ Scripts/function_order_validator.py
@@ -50,7 +50,6 @@ import os  # For running a command in the terminal
 import platform  # For getting the operating system name
 import sys  # For system-specific parameters and functions
 from colorama import Style  # For coloring the terminal
-from Logger import Logger  # For logging output to both terminal and file
 from pathlib import Path  # For handling file paths
 from typing import Any, Dict, List  # For type hinting
 
