510981d29da5ddcfc903ad066aedf732586c6ea6
Breno Farias da Silva <breno.vicio78@gmail.com>
2026-01-30 17:10:44 -0300

    REFACTOR: Updating the Constants in ./imports_placement.py

--- Scripts/imports_placement.py
+++ Scripts/imports_placement.py
@@ -78,11 +78,23 @@ class BackgroundColors:  # Colors for the terminal
 # Execution Constants:
 VERBOSE = False  # Set to True to output verbose messages
 
-# Telegram Bot Setup:
-TELEGRAM_BOT = None  # Global Telegram bot instance (initialized in setup_telegram_bot)
+# Project paths and settings
+ROOT_DIR = str(Path(__file__).resolve().parent.parent)
+OUTPUT_FILE = os.path.abspath(os.path.join(Path(__file__).resolve().parent, "imports_placement_report.json"))
+IGNORE_DIRS = {  # Directories to ignore
+    ".assets",
+    ".git",
+    ".github",
+    ".idea",
+    "__pycache__",
+    "Datasets",
+    "env",
+    "Logs",
+    "venv",
+}
 
 # Logger Setup:
-logger = Logger(f"./Logs/{Path(__file__).stem}.log", clean=True)  # Create a Logger instance
+logger = Logger(f"../Logs/{Path(__file__).stem}.log", clean=True)  # Create a Logger instance
 sys.stdout = logger  # Redirect stdout to the logger
 sys.stderr = logger  # Redirect stderr to the logger
 
