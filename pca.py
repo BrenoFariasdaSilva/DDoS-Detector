3acc8156c08a7bcbaa302b10f24198d827012952
Breno Farias da Silva <breno.vicio78@gmail.com>
2026-01-30 14:04:01 -0300

    FEAT: Adding the verify_dot_env_file function to ./pca.py

--- pca.py
+++ pca.py
@@ -160,6 +160,21 @@ def verbose_output(true_string="", false_string=""):
         print(false_string)  # Output the false statement string
 
 
+def verify_dot_env_file():
+    """
+    Verifies if the .env file exists in the current directory.
+
+    :return: True if the .env file exists, False otherwise
+    """
+
+    env_path = Path(__file__).parent / ".env"  # Path to the .env file
+    if not env_path.exists():  # If the .env file does not exist
+        print(f"{BackgroundColors.CYAN}.env{BackgroundColors.YELLOW} file not found at {BackgroundColors.CYAN}{env_path}{BackgroundColors.YELLOW}. Telegram messages may not be sent.{Style.RESET_ALL}")
+        return False  # Return False
+
+    return True  # Return True if the .env file exists
+
+
 def verify_filepath_exists(filepath):
     """
     Verify if a file or folder exists at the specified path.
