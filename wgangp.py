2b89e23c5c5cc49f85149582e2f832e62eb95ee2
Breno Farias <breno.vicio78@gmail.com>
2026-02-22 15:22:32 -0300

    REFACTOR: Minor changes in ./wgangp.py

--- wgangp.py
+++ wgangp.py
@@ -2094,9 +2094,9 @@ def compose_generation_start_message(n: int, args, generated_file_name: str, ori
                 ratio_info = ""  # Fallback if conversion fails
         
         if ratio_info != "":  # If ratio information is available
-            return f"Starting generation: producing {n} samples ({ratio_info}) to {generated_file_name}"  # Final formatted message
+            return f"Starting generation: Producing {n} samples ({ratio_info}) to {generated_file_name}"  # Final formatted message
         
-        return f"Starting generation: producing {n} samples to {generated_file_name}"  # Fallback message
+        return f"Starting generation: Oroducing {n} samples to {generated_file_name}"  # Fallback message
     
     except Exception as e:
         print(str(e))  # Print exception message
